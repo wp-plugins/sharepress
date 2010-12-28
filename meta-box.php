7d3af24cfbfd6b4569ce295260b370df13995217
aaroncollegeman <aaroncollegeman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-28 04:41:36 +0000

    Sorry for the messy commit - everything is right now: version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/sharepress/trunk@326527 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ meta-box.php
@@ -0,0 +1,27 @@
+<?php 
+/*
+sharepress
+Copyright (C)2010  Fat Panda LLC
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+if (!defined('ABSPATH')) exit; /* silence is golden... */ ?>
+
+<div style="padding:0px 7px; background-color: lightYellow; border: 1px solid #e6db55; border-radius: 3px;">
+  <p><b>Go pro!</b> This plugin can do more: a lot more. <a href="#">Learn more</a> or <a href="#">Buy now</a>.</p>
+</div>
+
+
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Plugin Name ===
+Contributors: aaroncollegeman
+Donate link: http://sharepress.fatpandadev.com
+Tags: facebook, social, sharing, automagical
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 1.0
+
+Automatically send your Posts to your personal Facebook Wall. 
+
+== Description ==
+
+Sharepress automatically sends your WordPress Posts to your personal Facebook Wall. 
+
+== Installation ==
+
+1. Extract the zip archive into the plugin directory. This should create a folder named 'sharepress-lite'
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Configure Sharepress. Instructions in the application will help you to create your own Facebook Application (required)
+
+== Frequently Asked Questions ==
+
+None yet!
+
+== Screenshots ==
+
+None yet!
+
+== Changelog ==
+
+= 1.0 =
+* The first release!
+
+== Upgrade Notice ==
+
+= 1.0 =
+Because it's the first version!
