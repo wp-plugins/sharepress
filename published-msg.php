0b1018ba1f62674061bb5979947c7261f090d21b
aaroncollegeman <aaroncollegeman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-10 17:14:14 +0000

    Tagging release 2.0
    git-svn-id: https://plugins.svn.wordpress.org/sharepress/trunk@421732 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- published-msg.php
+++ published-msg.php
@@ -1,34 +1,47 @@
-<?php 
-/*
-sharepress
-Copyright (C)2010-2011  Fat Panda LLC
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-if (!defined('ABSPATH')) exit; ?>
+<?php if (!defined('ABSPATH')) exit; ?>
 
 <fieldset>
   <legend>
-    <b>Published on Facebook</b>
+    <?php if ($posted) { ?>
+      <b>Last Published on Facebook</b>
+    <?php } else if ($scheduled) { ?>
+      <b>Scheduled to Post to Facebook</b>
+    <?php } else if ($last_posted) { ?>
+      <b>Last Published on Facebook</b>
+    <?php } ?>
   </legend>
-  <label style="display:inline-block;">
-    <p><em><?php echo htmlentities($meta['message']) ?></em></p>
-    <div style="margin-bottom:10px;"><?php echo date_i18n('M d, Y @ H:i', is_string($posted) ? strtotime($posted) : $posted, true) ?></div>
-    <input type="checkbox" id="sharepress_meta_publish_again" name="sharepress_meta[publish_again]" value="1" /> 
-    Publish to Facebook again
+  <label style="display:inline-block; width:100%;">
+    <p style="margin-bottom:12px; color:#555; width:235px; overflow:hidden;">
+      <?php echo htmlentities($scheduled ? $meta['message'] : $last_result['message']) ?>
+    </p>
+    <div style="width:100%;">
+      <?php if (Sharepress::$pro) { ?>
+        <a class="button" id="btn_publish_again" style="float:right; position:relative; top:-6px; margin-bottom:-6px; <?php if (@$_GET['sharepress'] == 'schedule') echo 'display:none;' ?>" href="#" onclick="sharepress_publish_again(); return false;">
+          <?php if ($posted) { ?>
+            Publish Again
+          <?php } else if ($scheduled) { ?>
+            Edit
+          <?php } else if ($last_posted) { ?>
+            Publish Again
+          <?php } ?>
+        </a>
+      <?php } ?>
+      <?php if ($posted) { ?>
+        <span><?php echo date_i18n('M d, Y @ H:i', ( is_numeric($posted) ? $posted : strtotime($posted) ) + ( get_option( 'gmt_offset' ) * 3600 ), true) ?></span>
+      <?php } else if ($scheduled) { ?>
+        <span><?php echo date_i18n('M d, Y @ H:i', ( is_numeric($scheduled) ? $scheduled : strtotime($scheduled) ), true) ?></span>
+      <?php } else if ($last_posted) { ?>  
+        <span><?php echo date_i18n('M d, Y @ H:i', $last_posted + ( get_option( 'gmt_offset' ) * 3600 ), true) ?></span>
+      <?php } ?>
+    </div>
+    <?php if (!Sharepress::$pro) { ?>
+      <div style="padding:12px 0 2px;">
+        <input type="checkbox" id="sharepress_meta_publish_again" name="sharepress_meta[publish_again]" value="1" /> 
+        Publish Again
+      </div>
+    <?php } else { ?>
+      <input type="hidden" id="sharepress_meta_publish_again" name="sharepress_meta[publish_again]" value="<?php echo @$_GET['sharepress'] == 'schedule' ? 1 : 0 ?>" />
+      <input type="hidden" id="sharepress_meta_cancelled" name="sharepress_meta[cancelled]" value="0" />
+    <?php } ?>
   </label>
 </fieldset>
-<br />
